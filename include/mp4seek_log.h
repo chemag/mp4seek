76ea64207a4bb2ac04af57c6bda00c17c74af206
Chema Gonzalez <chemag@meta.com>
2026-02-25 13:29:50 -0800

    mp4seek: add support for building in Android
    Add Android.bp and logging glue.

--- include/mp4seek_log.h
+++ include/mp4seek_log.h
@@ -1,7 +1,24 @@
 #pragma once
 
 // Platform-dependent logging for mp4seek.
+// On Android, uses __android_log_print (logcat).
 // On Linux/desktop, uses fprintf(stderr, ...).
+
+#ifdef __ANDROID__
+
+#include <android/log.h>
+
+#define MP4SEEK_LOG_TAG "mp4seek"
+#define MP4SEEK_LOGE(fmt, ...) \
+  __android_log_print(ANDROID_LOG_ERROR, MP4SEEK_LOG_TAG, fmt, ##__VA_ARGS__)
+#define MP4SEEK_LOGW(fmt, ...) \
+  __android_log_print(ANDROID_LOG_WARN, MP4SEEK_LOG_TAG, fmt, ##__VA_ARGS__)
+#define MP4SEEK_LOGI(fmt, ...) \
+  __android_log_print(ANDROID_LOG_INFO, MP4SEEK_LOG_TAG, fmt, ##__VA_ARGS__)
+#define MP4SEEK_LOGD(fmt, ...) \
+  __android_log_print(ANDROID_LOG_DEBUG, MP4SEEK_LOG_TAG, fmt, ##__VA_ARGS__)
+
+#else
 
 #include <stdio.h>
 
@@ -9,3 +26,5 @@
 #define MP4SEEK_LOGW(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
 #define MP4SEEK_LOGI(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
 #define MP4SEEK_LOGD(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
+
+#endif
